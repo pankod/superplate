af8ff88365e605c6dc1779099624b2e22e080067
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-01-15 11:17:14 +0300

    update next.config for css

--- template/next.config.js
+++ template/next.config.js
@@ -1,11 +1,11 @@
 const withPlugins = require('next-compose-plugins');
 
-<%_ if (css_features === 'less' || ui === "antd") { _%>
+<%_ if (css_features === 'less' || (ui === "antd" && css_features === 'less')) { _%>
     const withLess = require('@zeit/next-less')
 <%_ } _%>
 
 module.exports = withPlugins([
-    <%_ if (css_features === 'less' || ui === "antd") { _%>
+    <%_ if (css_features === 'less' || (ui === "antd" && css_features === 'less')) { _%>
         [withLess, 
             <%_ if (ui === "antd") { _%>
               {
