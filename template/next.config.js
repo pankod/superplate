d370586a1f2692605aba266a793fa61d4ae9b906
burcukaragozzz <33669461+burcukaragozzz@users.noreply.github.com>
2021-01-15 11:35:30 +0300

    antd plugin (#68)
    * update next.config for antd
    
    * update next.config for css

--- template/next.config.js
+++ template/next.config.js
@@ -1,11 +1,21 @@
 const withPlugins = require('next-compose-plugins');
 
-<%_ if (css_features === 'less') { _%>
+<%_ if (css_features === 'less' || (ui === "antd" && css_features === 'less')) { _%>
     const withLess = require('@zeit/next-less')
 <%_ } _%>
 
 module.exports = withPlugins([
-    <%_ if (css_features === 'less') { _%>
-        [withLess, { cssModules: true }],
+    <%_ if (css_features === 'less' || (ui === "antd" && css_features === 'less')) { _%>
+        [withLess, 
+            <%_ if (ui === "antd") { _%>
+              {
+                lessLoaderOptions: {
+                    javascriptEnabled: true,
+                },
+              },
+            <%_ } else { _%>
+                { cssModules: true }
+            <%_ } _%>
+        ],
     <%_ } _%>
 ]);
