f06b206b0288e530c7c58622ded9853cf5cfba72
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-01-18 16:25:47 +0300

    Add next-css plugin.

--- template/next.config.js
+++ template/next.config.js
@@ -3,19 +3,28 @@ const withPlugins = require('next-compose-plugins');
 <%_ if (css_features === 'less' || (ui === "antd" && css_features === 'less')) { _%>
     const withLess = require('@zeit/next-less')
 <%_ } _%>
+<%_ if (ui === "bootstrap" && css_features === 'less') { _%>
+    const withCss = require('@zeit/next-css')
+<%_ } _%>
 
-module.exports = withPlugins([
-    <%_ if (css_features === 'less' || (ui === "antd" && css_features === 'less')) { _%>
-        [withLess, 
-            <%_ if (ui === "antd") { _%>
-              {
-                lessLoaderOptions: {
-                    javascriptEnabled: true,
+module.exports = withPlugins(
+    [
+        <%_ if (css_features === 'less' || (ui === "antd" && css_features === 'less')) { _%>
+            [withLess, 
+                <%_ if (ui === "antd") { _%>
+                {
+                    lessLoaderOptions: {
+                        javascriptEnabled: true,
+                    },
                 },
-              },
-            <%_ } else { _%>
-                { cssModules: true }
-            <%_ } _%>
-        ],
-    <%_ } _%>
-]);
+                <%_ } else { _%>
+                    { cssModules: true }
+                <%_ } _%>
+            ],
+        <%_ } _%>
+
+        <%_ if (ui === "bootstrap" && css_features === 'less') { _%>
+            [withCss],
+        <%_ } _%>
+    ]
+);
