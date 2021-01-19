57238da8b47e50b66fc78502b4419cfdf6710591
Ali Emir Şen <senaliemir@gmail.com>
2021-01-19 16:04:58 +0300

    i18n configs (#76)
    * add next-translate config to template
    
    * add next-i18next config and _app HOC
    
    * fix nextconfig additional config condition
    
    * Update template/next.config.js
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update template/next.config.js
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

--- template/next.config.js
+++ template/next.config.js
@@ -14,15 +14,34 @@ const withPlugins = require('next-compose-plugins');
     })
 <%_ } _%>
 
-<%_ if (features.find(f => f === 'reverse-proxy')) { _%>
-    const config = {
-        devServer: {
-            proxy: {
-            '/api': 'http://localhost:3000'
-            }
-        }
-    };
+<%_ if (i18n === 'next-translate') { _%>
+    const nextTranslate = require('next-translate');
 <%_ } _%>
+
+<%_ if (i18n === 'next-i18next') { _%>
+    const { nextI18NextRewrites } = require('next-i18next/rewrites');
+
+    const localeSubpaths = {
+        tr: 'tr',
+        en: 'en'
+    }
+<%_ } _%>
+
+const config = {
+<%_ if (features.find(f => f === 'reverse-proxy')) { _%>
+    devServer: {
+        proxy: {
+        '/api': 'http://localhost:3000'
+        }
+    }, 
+<%_ } _%>
+<%_ if (i18n === 'next-i18next') { _%>
+    rewrites: async () => nextI18NextRewrites(localeSubpaths),
+    publicRuntimeConfig: {
+      localeSubpaths,
+    },
+<%_ } _%>
+};
 
 module.exports = withPlugins(
     [
@@ -47,8 +66,10 @@ module.exports = withPlugins(
         <%_ if (features.find(f => f === "bundle-analyzer")) { _%>
             [withBundleAnalyzer],
         <%_ } _%>
+
+        <%_ if (i18n === 'next-translate') { _%>
+            [nextTranslate],
+        <%_ } _%>
     ],
-    <%_ if (features.find(f => f === "reverse-proxy")) { _%>
-        config,
-    <%_ } _%>
+    config,
 );
