8b8909ae32493cef57ea6eb437bce87248154b6e
Muharrem <mkocadere@gmail.com>
2021-01-25 12:42:41 +0300

    add data-test attribute to default template

--- template/pages/index.tsx
+++ template/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <main>
             <h1><%= name %></h1>
-            <p>next cli prototype app</p>
+            <p <% if(!(e2etest === "none")) { %>  data-test="main-heading" <% } %> >next cli prototype app</p>
         </main>
     );
 }
