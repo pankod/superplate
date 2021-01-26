3c15172a0a42597aafe95e390d776e75a5d27ee9
mhrrmk <mkocadere@gmail.com>
2021-01-26 14:04:45 +0300

    Add Ant Design docs (#121)
    * add antd docs
    
    * fix config code
    
    * add nextjs caution. fix config code

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -2,6 +2,7 @@ module.exports = {
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
+    "UI Frameworks":['antd'],
     "CSS Preprocessors":['css', 'scss']
   },
 };
