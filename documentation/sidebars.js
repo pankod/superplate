d55328431e4e528b472e515b71b857538632d68a
Ali Emir Sen <senaliemir@gmail.com>
2021-01-27 16:59:57 +0300

    add developers guide to sidebar

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -3,6 +3,11 @@ module.exports = {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['fetch', 'axios', "apollo-graphql"],
     "UI Frameworks":['antd', 'bootstrap', 'tailwind', 'chakra-ui'],
-    "CSS Preprocessors":['css', 'scss', 'styled-components']
+    "CSS Preprocessors":['css', 'scss', 'styled-components'],
+    "Getting Started": [
+      "getting-started/how-it-works",
+      "getting-started/creating-a-plugin",
+      "getting-started/references",
+  ],
   },
 };
