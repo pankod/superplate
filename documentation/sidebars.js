ca37d0be63c415310ad5bdcc280de6395bd1e5bd
Necati Özmen <necati@pankod.com>
2021-01-27 18:04:44 +0300

    Add graphql-request doc (#140)
    * Add grapql-request doc
    
    * Edit doc
    
    * Update readme
    
    * Update documentation/docs/graphql-request.md
    
    Co-authored-by: mhrrmk <mkocadere@gmail.com>
    
    Co-authored-by: mhrrmk <mkocadere@gmail.com>

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['fetch', 'axios', "apollo-graphql"],
+    Features: ['fetch', 'axios', "apollo-graphql", "graphql-request"],
     "UI Frameworks":['antd', 'bootstrap', 'tailwind', 'chakra-ui'],
     "CSS Preprocessors":['css', 'scss', 'styled-components']
   },
