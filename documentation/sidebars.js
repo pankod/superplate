92ad8f4c13b317e3f6babf6f7cfd1c2a18cd5f90
Necati Özmen <necati@pankod.com>
2021-01-27 14:40:25 +0300

    Doc Apollo GraphQL  (#129)
    * Add apollo documentation
    
    * Remove space
    
    * Edit text
    
    * Add document link

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    Features: ['mdx', "apollo-graphql"],
     "UI Frameworks":['antd', 'bootstrap', 'tailwind', 'chakra-ui'],
     "CSS Preprocessors":['css', 'scss', 'styled-components']
   },
