d08c158f024f21e6dbddeb83435bea02108f985f
mhrrmk <mkocadere@gmail.com>
2021-01-28 15:44:07 +0300

    Add SWR docs (#145)
    * add swr docs
    
    * Update documentation/docs/hooks/swr.md
    
    Co-authored-by: Necati Özmen <necati@pankod.com>
    
    * Update documentation/docs/hooks/swr.md
    
    Co-authored-by: Necati Özmen <necati@pankod.com>
    
    Co-authored-by: Necati Özmen <necati@pankod.com>

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -3,6 +3,7 @@ module.exports = {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['fetch', 'axios', 'svgr', 'env', 'apollo-graphql', 'graphql-request'],
     "UI Frameworks":['antd', 'bootstrap', 'tailwind', 'chakra-ui'],
-    "CSS Preprocessors":['css', 'scss', 'styled-components']
+    "CSS Preprocessors":['css', 'scss', 'styled-components'],
+    Hooks: ['hooks/swr']
   },
 };
