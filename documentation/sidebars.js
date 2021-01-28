382220b8a6095125dbceb9fd0b763542fc41386a
Muharrem <mkocadere@gmail.com>
2021-01-28 15:04:33 +0300

    add redux docs

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -3,6 +3,7 @@ module.exports = {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['fetch', 'axios', 'svgr', 'apollo-graphql', 'graphql-request'],
     "UI Frameworks":['antd', 'bootstrap', 'tailwind', 'chakra-ui'],
-    "CSS Preprocessors":['css', 'scss', 'styled-components']
+    "CSS Preprocessors":['css', 'scss', 'styled-components'],
+    "State Management": ['state-management/redux']
   },
 };
