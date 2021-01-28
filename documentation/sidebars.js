efacbc0c8e4ce3e69722263ba4fcec010a6f9fdd
Necati Özmen <necati@pankod.com>
2021-01-28 13:39:42 +0300

    Add env doc (#144)
    * Add env doc
    
    * Update documentation/sidebars.js
    
    Co-authored-by: mhrrmk <mkocadere@gmail.com>
    
    Co-authored-by: mhrrmk <mkocadere@gmail.com>

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['fetch', 'axios', 'svgr', 'apollo-graphql', 'graphql-request'],
+    Features: ['fetch', 'axios', 'svgr', 'env', 'apollo-graphql', 'graphql-request'],
     "UI Frameworks":['antd', 'bootstrap', 'tailwind', 'chakra-ui'],
     "CSS Preprocessors":['css', 'scss', 'styled-components']
   },
