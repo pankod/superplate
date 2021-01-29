32202b19a23521e849375946ef680daa885aa6c7
Ali Emir Şen <senaliemir@gmail.com>
2021-01-29 09:37:26 +0300

    docs - getting started (#142)
    * add references
    
    * add how it works
    
    * wip - create a plugin
    
    * add developers guide to sidebar
    
    * Update documentation/docs/getting-started/creating-a-plugin.md
    
    Co-authored-by: Necati Özmen <necati@pankod.com>
    
    * wip create a plugin
    
    * Update documentation/docs/getting-started/how-it-works.md
    
    Co-authored-by: Necati Özmen <necati@pankod.com>
    
    * Update documentation/docs/getting-started/how-it-works.md
    
    Co-authored-by: Necati Özmen <necati@pankod.com>
    
    * Update documentation/docs/getting-started/how-it-works.md
    
    Co-authored-by: Necati Özmen <necati@pankod.com>
    
    * Update documentation/docs/getting-started/references.md
    
    Co-authored-by: Necati Özmen <necati@pankod.com>
    
    * Update documentation/docs/getting-started/references.md
    
    Co-authored-by: mhrrmk <mkocadere@gmail.com>
    
    * update styled-components url
    
    * package.js step
    
    * wip templates section in references
    
    * create a plugin article
    
    Co-authored-by: Necati Özmen <necati@pankod.com>
    Co-authored-by: mhrrmk <mkocadere@gmail.com>

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -4,6 +4,11 @@ module.exports = {
     Features: ['fetch', 'axios', 'svgr', 'env', 'reverse-proxy', 'apollo-graphql', 'graphql-request'],
     "UI Frameworks":['antd', 'bootstrap', 'tailwind', 'chakra-ui'],
     "CSS Preprocessors":['css', 'scss', 'styled-components'],
-    Hooks: ['hooks/swr']
+    "Getting Started": [
+      "getting-started/how-it-works",
+      "getting-started/creating-a-plugin",
+      "getting-started/references",
+  ],
+    Hooks: ['hooks/swr'],
   },
 };
