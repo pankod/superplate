ca422a22b8614d479f6c85c60b75434fbf77782e
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-02-01 15:06:35 +0300

    add sidebar

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -1,15 +1,16 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['fetch', 'axios', 'storybook', 'svgr', 'env', 'reverse-proxy','bundle-analyzer', 'apollo-graphql', 'graphql-request'],
+    "Docusaurus": ['doc1', 'doc2', 'doc3'],
+    "Features": ['fetch', 'axios', 'storybook', 'svgr', 'env', 'reverse-proxy','bundle-analyzer', 'apollo-graphql', 'graphql-request'],
     "UI Frameworks":['antd', 'bootstrap', 'tailwind', 'chakra-ui'],
     "CSS Preprocessors":['css', 'scss', 'styled-components'],
     "State Management": ['state-management/redux', 'state-management/recoil'],
+    "Hooks": ['hooks/swr', 'hooks/react-query', "hooks/react-use"],
+    "Linting Tools": ['linter/prettier'],
     "Getting Started": [
       "getting-started/how-it-works",
       "getting-started/creating-a-plugin",
       "getting-started/references",
     ],
-    Hooks: ['hooks/swr', 'hooks/react-query', "hooks/react-use"],
   },
 };
