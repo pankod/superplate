1a697e83acdc0c435dbd1c1f63b1a8d7fccbf572
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-02-03 14:49:10 +0300

    create features.js

---
+++ documentation/src/pages/features.js
@@ -0,0 +1,118 @@
+export const features = [
+    {
+        title: 'Tailwind CSS',
+        imageUrl: 'img/tailwind.svg',
+    },
+    {
+        title: 'Bootstrap',
+        imageUrl: 'img/bootstrap.svg',
+    },
+    {
+        title: 'Chakra UI',
+        imageUrl: 'img/chakra-ui.svg',
+    },
+    {
+        title: 'Antd',
+        imageUrl: 'img/antd.svg',
+    },
+    {
+        title: 'CSS',
+        imageUrl: 'img/css.svg',
+    },
+    {
+        title: 'SASS/SCSS',
+        imageUrl: 'img/sass.svg',
+    },
+    {
+        title: 'styled-components',
+        imageUrl: 'img/styled-components.png',
+    },
+    {
+        title: 'styled-system',
+        imageUrl: 'img/styled-system.svg',
+    },
+    {
+        title: 'Storybook',
+        imageUrl: 'img/storybook.svg',
+    },
+    {
+        title: 'svgr',
+        imageUrl: 'img/svgr.png',
+    },
+    {
+        title: 'Apollo GraphQL',
+        imageUrl: 'img/apollo.svg',
+    },
+    {
+        title: 'graphql-request',
+        imageUrl: 'img/graphql.svg',
+    },
+    {
+        title: 'SWR',
+        imageUrl: 'img/swr.svg',
+    },
+    {
+        title: 'React Query',
+        imageUrl: 'img/react-query.svg',
+    },
+    {
+        title: 'Redux',
+        imageUrl: 'img/redux.svg',
+    },
+    {
+        title: 'MobX',
+        imageUrl: 'img/mobx.svg',
+    },
+    {
+        title: 'Mobx State Tree',
+        imageUrl: 'img/mobx-state-tree.svg',
+    },
+    {
+        title: 'next-translate',
+        imageUrl: 'img/next-translate.svg',
+    },
+    {
+        title: 'next-i18next',
+        imageUrl: 'img/i18next.svg',
+    },
+    {
+        title: 'ESLint',
+        imageUrl: 'img/eslint.svg',
+    },
+    {
+        title: 'Prettier',
+        imageUrl: 'img/prettier.svg',
+    },
+    {
+        title: 'Enzyme',
+        imageUrl: 'img/enzyme.png',
+    },
+    {
+        title: 'Jest',
+        imageUrl: 'img/jest.svg',
+    },
+    {
+        title: 'React Testing Library',
+        imageUrl: 'img/testing-libraries.svg',
+    },
+    {
+        title: 'Cypress',
+        imageUrl: 'img/cypress.svg',
+    },
+    {
+        title: 'WebdriverIO',
+        imageUrl: 'img/webdriverio.svg',
+    },
+    {
+        title: 'Docker',
+        imageUrl: 'img/docker.svg',
+    },
+    {
+        title: 'GitHub Actions',
+        imageUrl: 'img/github-actions.svg',
+    },
+    {
+        title: 'Travis',
+        imageUrl: 'img/travis.svg',
+    },
+];
