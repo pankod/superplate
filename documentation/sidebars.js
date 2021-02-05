895134db7070571ca213367226e1a0757e77568e
Ali Emir Şen <senaliemir@gmail.com>
2021-02-05 09:36:50 +0300

    match order with prompts (#201)
    * match order with prompts
    
    * Update documentation/sidebars.js
    
    Co-authored-by: burcukaragozzz <33669461+burcukaragozzz@users.noreply.github.com>
    
    Co-authored-by: burcukaragozzz <33669461+burcukaragozzz@users.noreply.github.com>

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -1,21 +1,21 @@
 module.exports = {
   someSidebar: {
     "Getting Started": ['quick-start'],
-    "Features": ['fetch', 'axios', 'storybook', 'svgr', 'env', 'reverse-proxy','bundle-analyzer', 'apollo-graphql', 'graphql-request'],
-    "UI Frameworks":['antd', 'bootstrap', 'tailwind', 'chakra-ui'],
+    "UI Frameworks":['tailwind', 'bootstrap', 'chakra-ui', 'antd'],
     "CSS Preprocessors":['css', 'scss', 'styled-components'],
-    "State Management": ['state-management/redux-toolkit', 'state-management/recoil', 'state-management/mobx'],
+    "Features": ['fetch', 'axios', 'storybook', 'svgr', 'env', 'reverse-proxy','bundle-analyzer', 'apollo-graphql', 'graphql-request'],
     "Hooks": ['hooks/swr', 'hooks/react-query', "hooks/react-use"],
-    "Linting Tools": ['linter/eslint', 'linter/prettier', 'linter/lint-staged'],
+    "State Management": ['state-management/redux-toolkit', 'state-management/recoil', 'state-management/mobx'],
     "i18n": ['i18n/next-translate', 'i18n/next-i18next'],
+    "Linting Tools": ['linter/eslint', 'linter/prettier', 'linter/lint-staged'],
     "Testing": ['testing/jest', 'testing/testing-library', 'testing/enzyme'],
+    "E2E Testing": ['e2e-testing/cypress', 'e2e-testing/wdio'],
     "Deployment": ["docker"],
+    "Continuous integration": ["ci/github-actions", "ci/travis"],
     "Development": [
       'development/how-it-works',
       'development/creating-a-plugin',
       'development/references',
-    ],
-    "E2E Testing": ['e2e-testing/cypress', 'e2e-testing/wdio'],
-    "Continuous integration": ["ci/github-actions", "ci/travis"]
+    ]
   },
 };
