b9239d334e755de0e1f25149b695fe316ecdb1b1
burcukaragozzz <33669461+burcukaragozzz@users.noreply.github.com>
2021-02-05 09:39:42 +0300

    Landing design (#184)
    * add icons
    
    * edit config file and update header and footer
    
    * change tagline text
    
    * update github link
    
    * updated base url
    
    * edited hero banner
    
    * add icons
    
    * create features.js
    
    * logos content area created
    
    * links to icons added
    
    * edit mobx-state-tree svg
    
    * updates enzymeand recoil logos
    
    * updated features
    
    * add badges
    
    * edit className
    
    * fix build bug fix
    
    * fixed features undefined bug
    
    * fixs
    
    * moved features.js
    
    * edit responsive mobile styles
    
    * add sections
    
    * react-bootstrap added features
    
    * features added footer
    
    * fixed footer quick-start path bug
    
    * redux-toolkit path changed config.js
    
    * fixed mobile size
    
    * add darkMode
    
    * added pankod github link to footer logo
    
    * add stars badge

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -1,70 +1,128 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
+import CodeBlock from '@theme/CodeBlock';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+
+import { features } from '../features';
 import styles from './styles.module.css';
 
-const features = [
-  {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
-
-function Feature({imageUrl, title, description}) {
+function Badges() {
+  return (
+    <div className={styles.topBadges}>
+      <a href="https://meercode.io/pankod/superplate">
+        <img src="https://api.meercode.io/badge/pankod/superplate?type=ci-score&token=IITTyA0OnDnR3phwZbp61uZW9QO05lxQ&lastDay=14" alt="Meercode CI Score" />
+      </a>
+      <a href="https://meercode.io/pankod/superplate">
+        <img src="https://api.meercode.io/badge/pankod/superplate?type=ci-success-rate&token=IITTyA0OnDnR3phwZbp61uZW9QO05lxQ&lastDay=14" alt="Meercode CI Success Rate" />
+      </a>
+      <a href="https://david-dm.org/pankod/superplate">
+        <img src="https://david-dm.org/pankod/superplate/status.svg" alt="Dependencies Status" />
+      </a>
+      <a href="https://david-dm.org/pankod/superplate?type=dev">
+        <img src="https://david-dm.org/pankod/superplate/dev-status.svg" alt="devDependencies Status" />
+      </a>
+      <a href="https://travis-ci.org/pankod/superplate">
+        <img src="https://travis-ci.org/pankod/superplate.svg?branch=master" alt="devDependency Status" />
+      </a>
+      <a href="https://img.shields.io/npm/dw/@pankod/superplate">
+        <img src="https://img.shields.io/npm/dw/@pankod/superplate" alt="npm" />
+      </a>
+    </div>
+  )
+}
+function Feature({ imageUrl, title, url }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
+    <div className={clsx('col col--2', styles.feature)}>
+      <a href={url} target="_blank">
+        {imgUrl && (
+          <div className="text--center">
+            <img className={styles.featureImage} src={imgUrl} alt={title} />
+          </div>
+        )}
+        <h3 className={styles.featureTitle}>{title}</h3>
+      </a>
     </div>
+  );
+}
+
+function Sections() {
+  return (
+    <>
+      <div className={styles.gettingStartedSection}>
+        <div className="container padding-vert--xl text--left">
+          <div className="row">
+            <div className="col col--4 col--offset-1">
+              <h2>All plugins has best practice</h2>
+              <p>
+                We have added all the <strong>best practices</strong> you need while creating your new project.
+                superplate gives you many abilities to create your own plugin and interact with the others. 
+                <br />
+                <br />
+                To create a project called <i>my-app</i>, run this command:
+              </p>
+              <CodeBlock className="language-sh">
+                npx superplate my-app
+              </CodeBlock>
+              <br />
+            </div>
+            <div className="col col--5 col--offset-1">
+              <img
+                className={styles.sectionImage}
+                alt="Easy to get started in seconds"
+                src={
+                  'https://user-images.githubusercontent.com/11361964/106468355-dd9be280-64ae-11eb-82da-c313beb16d6b.gif'
+                }
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="container padding-vert--xl text--left">
+          <div className="row">
+            <div className="col col--4 col--offset-1">
+              <img
+                className={styles.sectionImage}
+                alt="Easy to update"
+                src={useBaseUrl('img/code.svg')}
+              />
+            </div>
+            <div className="col col--5 col--offset-1">
+              <h2>Easy to extend/customize plugin structure</h2>
+              <p>
+                Kolay extend/customize edilebilir plugin yapısı
+                superplate gives you many abilities to create your own plugin and interact with the others. 
+              </p>
+              <CodeBlock className="language-sh">
+                npm run dev
+              </CodeBlock>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.logoContainer}>
+            <img className={clsx(styles.heroBannerLogo, 'margin-vert--md')} src="img/superplate-logo.svg" alt="logo-icon" />
+          </div>
+          <img className={styles.heroTitle} src="img/superplate-text.svg" alt="logo-text" />
+          <p className={clsx(styles.heroSubtitle, 'hero__subtitle')}>{siteConfig.tagline}</p>
+          <Badges />
           <div className={styles.buttons}>
             <Link
               className={clsx(
@@ -78,8 +136,8 @@ function Home() {
         </div>
       </header>
       <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
+        {(features ?? []) && features.length > 0 && (
+          <section className={styles.featuresContainer}>
             <div className="container">
               <div className="row">
                 {features.map((props, idx) => (
@@ -90,6 +148,7 @@ function Home() {
           </section>
         )}
       </main>
+      <Sections />
     </Layout>
   );
 }
