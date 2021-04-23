5338e0f34566704eeb9bb781cf759d11507fc412
Ömer Faruk APLAK <omer@pankod.com>
2021-04-23 10:43:01 +0300

    add product hunt badges

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -12,6 +12,7 @@ import styles from './styles.module.css';
 function Badges() {
   return (
     <div className={styles.topBadges}>
+      <a href="https://www.producthunt.com/posts/superplate?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-superplate" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=293252&theme=light" alt="Superplate - The frontend boilerplate with superpowers | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
       <a href="https://meercode.io">
         <img src="https://meercode.io/badge/pankod/superplate?type=ci-score&branch=master&token=2ZiT8YsoJgt57JB23NYwXrFY3rJHZboT&lastDay=31" alt="Meercode CI Score" />
       </a>
