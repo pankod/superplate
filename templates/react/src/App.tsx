bb8e9230527d6b3390da0e9af732cc0ca0a64d03
Ömer Faruk APLAK <omer@pankod.com>
2021-04-27 16:51:43 +0300

    update react template

---
+++ templates/react/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+    return (
+        <div className="App">
+            test
+        </div>
+      );
+};
+
+export default App;
--- templates/react/src/index.tsx
+++ templates/react/src/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+<% - _app.import.join("\n") _ %>
 
-ReactDOM.render(
-  <React.StrictMode>
-      <main>
-            <h1><%= name %></h1>
-            <p>react prototype app</p>
-        </main>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+<%
+    var top = _app.wrapper.map(wrapper => wrapper[0] || "");
+    var bottom = _app.wrapper.map(wrapper => wrapper[1] || "").reverse();
+%>
+
+  ReactDOM.render(() => {
+  <% - _app.inner.join("\n") %>
+
+  return (
+      <React.StrictMode>
+        <%- top.join("\n") %>
+        <App />
+        <%- bottom.join("\n") %>
+      </React.StrictMode>
+    );
+  },
+    document.getElementById('root')
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
