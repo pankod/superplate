f1df0b58bcf7c915df2fab3e4905f6067e57e939
Aydın Akyol <aydinakyol11@gmail.com>
2021-04-27 16:08:50 +0300

    add react support

---
+++ templates/react/src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <main>
+            <h1><%= name %></h1>
+            <p>react prototype app</p>
+        </main>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
