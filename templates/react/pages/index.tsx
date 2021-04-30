4971e33493c83b9ed765190df531e6d24fa7561a
Aydın <aydinakyol11@gmail.com>
2021-04-30 09:32:33 +0300

    Update templates/react/pages/index.tsx
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

--- templates/react/pages/index.tsx
+++ templates/react/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <main>
             <h1><%= name %></h1>
-            <p>react cli prototype app</p>
+            <p>superplate react app</p>
         </main>
     );
 }
