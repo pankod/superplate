6e71b2fa4ca9ace6b624ea4ee2ebfd1a4c02570e
Ali Emir Sen <senaliemir@gmail.com>
2022-06-24 01:26:03 +0300

    test: ignore `/lib`

--- jest.config.ts
+++ jest.config.ts
@@ -151,9 +151,7 @@ export default {
     // ],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    // testPathIgnorePatterns: [
-    //   "/node_modules/"
-    // ],
+    testPathIgnorePatterns: ["/node_modules/", "/lib"],
 
     // The regexp pattern or array of patterns that Jest uses to detect test files
     // testRegex: [],
