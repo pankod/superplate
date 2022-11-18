272258ff43998a900ef0a8839a29566c6e26b85f
Ömer Faruk APLAK <omer@pankod.com>
2022-11-18 15:32:14 +0300

    fix: update project descriptions

--- src/Helper/humanize/index.ts
+++ src/Helper/humanize/index.ts
@@ -6,35 +6,34 @@ export const HumanizeChoices = {
             case "react":
                 return {
                     title: "React",
-                    description:
-                        "Creates a React CRA project without SSR support",
+                    description: "Creates a React CRA project",
                     value: choice,
                 };
             case "nextjs":
                 return {
                     title: "NextJS",
-                    description: "Creates a Next.js project with SSR support",
+                    description: "Creates a Next.js project",
                     value: choice,
                 };
             case "refine-react":
                 return {
                     title: "refine(CRA)",
                     description:
-                        "Creates a basic refine project without SRR support.",
+                        "Creates a basic refine project (Recommended for CRUD applications)",
                     value: choice,
                 };
             case "refine-nextjs":
                 return {
                     title: "refine(Next.js)",
                     description:
-                        "Creates a refine Next.js project with SSR support.",
+                        "Creates a refine Next.js project with SSR support (Recommended for CRUD applications).",
                     value: choice,
                 };
             case "refine-remix":
                 return {
                     title: "refine(Remix)",
                     description:
-                        "Creates a refine Remix project with SSR support.",
+                        "Creates a refine Remix project with SSR support (Recommended for CRUD applications)",
                     value: choice,
                 };
             default:
